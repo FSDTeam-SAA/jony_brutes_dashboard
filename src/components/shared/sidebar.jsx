da849375352a2a74b6863517000f0a4658f53fa8
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-11-15 11:51:03 +0600

    sidebar done

--- src/components/shared/sidebar.jsx
+++ src/components/shared/sidebar.jsx
@@ -1,13 +1,61 @@
+import { FaUserNurse } from "react-icons/fa6";
+import { MdOutlineAdUnits, MdOutlineDesignServices, MdOutlineRateReview } from "react-icons/md";
+import { NavLink } from "react-router";
+
 const Sidebar = () => {
   const items = [
     {
-      path: "/dashboard/commander",
+      path: "/dashboard",
       label: "Commander",
-      icon: ""
+      icon: FaUserNurse,
+    },
+    {
+      path: "/dashboard/reviews",
+      label: "Reviews",
+      icon: MdOutlineRateReview,
+    },
+    {
+      path: "/dashboard/service",
+      label: "Service",
+      icon: MdOutlineDesignServices,
+    },
+    {
+      path: "/dashboard/units",
+      label: "Units",
+      icon: MdOutlineAdUnits,
     },
   ];
 
-  return <div className="p-5">sidebar</div>;
+  return (
+    <div className="py-5 pl-5">
+      <div>logo</div>
+
+      <div className="mt-5 flex flex-col space-y-4">
+        {items.map((item, index) => (
+          <NavLink
+            key={index}
+            to={item.path}
+            className={({ isActive }) =>
+              `p-2 rounded-lg transition-colors ${
+                isActive
+                  ? "bg-primary text-white font-semibold"
+                  : "bg-gray-100 text-gray-700 hover:bg-gray-200"
+              }`
+            }
+          >
+            {({ isActive }) => (
+              <button className="flex items-center gap-2 w-full text-left h-[30px] cursor-pointer">
+                <item.icon
+                  className={isActive ? "text-white" : "text-gray-600"}
+                />
+                {item.label}
+              </button>
+            )}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -4,10 +4,10 @@ import Sidebar from "../components/shared/sidebar";
 const Layout = () => {
   return (
     <div className="flex">
-      <div className="border border-red-400 w-[12%]">
+      <div className="w-[12%]">
         <Sidebar />
       </div>
-      <div className="border border-green-400 flex-1 min-h-screen p-5">
+      <div className="flex-1 min-h-screen p-5">
         <Outlet />
       </div>
     </div>
