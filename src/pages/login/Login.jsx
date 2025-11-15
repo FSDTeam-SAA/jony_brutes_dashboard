7c679e03c81960648916aea2eca7f501488971a4
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-11-15 11:33:43 +0600

    react hook form add

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,26 @@
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      Login form
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="test" {...register("example")} />
+        <input {...register("exampleRequired", { required: true })} />
+        {errors.exampleRequired && <span className="text-red-500">This field is required</span>}
 
-export default Login
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
