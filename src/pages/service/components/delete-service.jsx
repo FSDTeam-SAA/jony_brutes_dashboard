442a72a38607b3b44ba8f265320822c83cfdb766
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-11-16 09:09:17 +0600

    delete done

--- src/pages/service/components/delete-service.jsx
+++ src/pages/service/components/delete-service.jsx
@@ -22,7 +22,13 @@ const DeleteService = ({ id }) => {
       queryclient.invalidateQueries({ queryKey: ["services"] });
     },
     onError: (error) => {
-      toast.error(error?.message);
+      const message =
+        error?.response?.data?.message ||
+        error?.response?.data?.error ||
+        error?.message ||
+        "Something went wrong";
+
+      toast.error(message);
     },
   });
 
